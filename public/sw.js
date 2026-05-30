0efdcfa3d31eface41dc8be197942796417c62b2
mohamedzakariyakone-cpu <mohamedzakariyakone@gmail.com>
2026-05-30 15:46:52 +0000

    mise a jour 3

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,8 @@
 // Fichier public/sw.js
-self.addEventListener('install', () => {
+
+const CACHE_NAME = 'hajj-v1';
+
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
@@ -7,7 +10,11 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
 });
 
-// Requis pour valider le critère d'installation PWA
-self.addEventListener('fetch', () => {
-  // Tu pourras ajouter de la mise en cache ici plus tard si nécessaire
+// Chrome exige une vraie logique de interception pour valider le critère PWA en ligne
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
